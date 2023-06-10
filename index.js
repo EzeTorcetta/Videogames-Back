79aeed3c5182dfde2808142b45b847772f83f84f
Ezequiel Torcetta <ezequiel.torcetta.97@gmail.com>
2023-06-10 12:43:10 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,10 @@
 
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-
+const port = process.env.PORT || 3001
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(3001, () => {
-    console.log('%s listening at 3001 (force: false)'); // eslint-disable-line no-console
+  server.listen(port, () => {
+    console.log(`Server raised in port ${port} (force: false)`); // eslint-disable-line no-console
   });
 });
